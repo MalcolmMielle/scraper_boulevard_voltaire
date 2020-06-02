3d88eb97ab0e43085e2ce45c3d168e474f57c188
Malcolm Mielle <malcolm.mielle@gmail.com>
2020-06-02 02:09:08 +0200

    statistics about the dataset

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # scraper_boulevard_voltaire
+
+A program to scrap data from [boulevard voltaire](www.bvoltaire.fr).
+
+Simply run `python3 scrapper.py`
---
+++ statistics.py
@@ -0,0 +1,44 @@
+from utils import read_functions
+import spacy
+from tqdm import tqdm
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+def tokenize(text_input: str, nlp_fr):
+    document = nlp_fr(text_input)
+    return document
+
+
+def sum_words(doc):
+    return len(doc)
+
+
+def process(doc):
+    """
+    Remove stop word and create lemma of word
+    :param doc: list of token in text
+    :return: list of word lemma
+    """
+    filtered_sent = []
+    for word in doc:
+        if not word.is_stop:
+            filtered_sent.append(word.lemma_)
+    return filtered_sent
+
+
+
+# use your path
+path = r'./data'
+frame = read_functions.read_csv(path)
+print(frame)
+
+nlp = spacy.load("fr_core_news_sm")
+tqdm.pandas()
+print("Tokenize")
+frame['tokenized'] = frame['text'].progress_apply(tokenize, args=(nlp,))
+print("Tokenized\nSum words")
+frame['sum'] = frame['tokenized'].progress_apply(sum_words)
+print("Tokenized\nSum words")
+
+print("Number of token:", frame['sum'].sum())
+
